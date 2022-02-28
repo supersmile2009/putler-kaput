0052b2ca2849414cecc59ce12b72af1c8c89020c
Alexandr Zolotukhin <supersmile2009@gmail.com>
2022-02-28 13:04:11 +0200

    Install missing packages

--- init.sh
+++ init.sh
@@ -4,9 +4,14 @@ container_count="${1}"
 
 # Using Docker CE directly provided by Docker
 echo "[INFO] [${SCRIPT}] Setup docker"
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 #sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
