4b91d8e9ab9fed010d8fe6f908af5f84dcd4a682
Alexandr Zolotukhin <supersmile2009@gmail.com>
2022-03-07 01:53:03 +0200

    Add docker-compose. Add concurrency auto-compute

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
+getRamMb() {
+    ram_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+    ram_mb=$(( ram_kb / 1024 ))
+    echo "${ram_mb}"
+}
+
+getMaxConcurrentProc() {
+  ram_mb=$(getRamMb)
+  proc_count=$(( (ram_mb - 400) / 200 ))
+  proc_count=$(( proc_count > 0 ? proc_count : 1 ))
+  echo "${proc_count}"
+}
+
+# first arg is `-f` or `--some-option` or no argument provided
+if [ "${1#-}" != "$1" ] || [ "${1}" = "" ]; then
+  c_opt_found="false"
+  for opt in "$@"; do
+    if [ "${opt}" != "${opt#-c}" ]; then
+      c_opt_found="true"
+    fi
+  done
+  if [ "${c_opt_found}" = "false" ]; then
+    max_proc=$(getMaxConcurrentProc)
+    set -- "-c${max_proc}" "$@"
+  fi
+
 	set -- /usr/local/bin/php /app/bin/console app:client:run "$@"
 fi
 
-exec "$@"
+echo "$@"
